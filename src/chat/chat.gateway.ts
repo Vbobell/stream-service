0dfc7833c7e564b4d5446a1b02ac904be9d204e8
Vbobell <vinicius.bobel41@gmail.com>
2020-06-24 09:34:43 -0300

    feat(chat): create chat module

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,26 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway {
+  messages: string[] = [];
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('send-message')
+  sendMessage(@MessageBody() data: string): Observable<WsResponse<string[]>> {
+    this.messages.push(data);
+
+    return from(this.messages).pipe(
+      map(() => ({ event: 'send-message', data: this.messages })),
+    );
+  }
+}
